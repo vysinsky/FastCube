5497cb2d06f1a37568171b6bdba047f5efbe3502
Michal Vyšinský <michal@vysinsky.cz>
2021-01-03 20:43:41 +0100

    Improve score UI and add Analytics

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace FastCube
+{
+    public class GameManager: MonoBehaviour
+    {
+
+        public static void PauseGame()
+        {
+            Debug.Log("PAUSE");
+            Time.timeScale = 0;
+        }
+
+        public static void ResumeGame()
+        {
+            Time.timeScale = 1;
+        }
+
+    }
+}

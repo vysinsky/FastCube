c16821b4752881989e6fbaecb06be415ccdf497b
Michal Vyšinský <michal@vysinsky.cz>
2021-01-14 19:00:41 +0100

    feat: initialize analytics

---
+++ Assets/Scripts/Behaviours/FirebaseManager.cs
@@ -0,0 +1,19 @@
+﻿using Firebase;
+using Firebase.Analytics;
+using UnityEngine;
+
+namespace FastCube.Behaviours
+{
+    public class FirebaseManager: MonoBehaviour
+    {
+
+        private void Start()
+        {
+            FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+            {
+                FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+            });
+        }
+
+    }
+}
